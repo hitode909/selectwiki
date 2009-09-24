996a22c49c4ccc640083b1f3c48f19fef2bd7d59
hitode909 <hitode909@gmail.com>
2009-09-25 00:36:07 +0900

    indexをcacheする

--- controller/index.py
+++ controller/index.py
@@ -1,14 +1,20 @@
 from google.appengine.ext import webapp
 from model import Word
+from google.appengine.api import memcache
 import os
 from google.appengine.ext.webapp import template
+import logging
 
 class IndexPage(webapp.RequestHandler):
   def get(self):
+    result = memcache.get("index")
+    if not result:
+      logging.info("cache not hit(index)")
+      template_values = {
+        'words': Word.all().fetch(1000)
+        }
+      path = os.path.join(os.path.dirname(__file__), '..', 'view', 'index.html')
+      result = template.render(path, template_values)
+      memcache.set("index", result, 60)
 
-    template_values = {
-      'words': Word.all().fetch(1000)
-      }
-
-    path = os.path.join(os.path.dirname(__file__), '..', 'view', 'index.html')
-    self.response.out.write(template.render(path, template_values))
+    self.response.out.write(result)
